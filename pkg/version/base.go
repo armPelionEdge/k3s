d482d463dd5fedebe46c1c39917f6617d2b67010
Jordan Rife <jordan.rife@arm.com>
2019-06-14 15:33:15 -0500

    v1.14.1-k3s-arm.2

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "14"
-	gitVersion   = "v1.14.1-k3s-arm.1"
-	gitCommit    = "6b1846bb80f2faeeab8a37c44a622c72665a3f4d"
+	gitVersion   = "v1.14.1-k3s-arm.2"
+	gitCommit    = "a7123cdc1bcd241563277c02106de5334f69f88c"
 	gitTreeState = "clean"
-	buildDate = "2019-06-14T18:27+00:00Z"
+	buildDate = "2019-06-14T20:33+00:00Z"
 )
